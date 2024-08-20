5b49992c7c7875890fc02a8c232e087dd2cf127d
solaire <141582502+hazardous-sun@users.noreply.github.com>
2024-08-20 13:57:40 -0300

    passed the definition of the menu to a function

--- main.go
+++ main.go
@@ -19,6 +19,31 @@ func main() {
 	// Create an instance of the app structure
 	app := NewApp()
 
+	// Set menu
+	setMenuBar(app)
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "Image Manipulation Tool",
+		Width:  1024,
+		Height: 768,
+		Menu:   AppMenu,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 35, G: 35, B: 35, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
+
+func setMenuBar(app *App) {
 	AppMenu := menu.NewMenu()
 	FileMenu := AppMenu.AddSubmenu("File")
 	FileMenu.AddText("&Open", keys.CmdOrCtrl("o"), func(_ *menu.CallbackData) {
@@ -58,24 +83,4 @@ func main() {
 	FileMenu.AddText("Exit", keys.CmdOrCtrl("q"), func(_ *menu.CallbackData) {
 		runtime.Quit(app.ctx)
 	})
-
-	// Create application with options
-	err := wails.Run(&options.App{
-		Title:  "Image Manipulation Tool",
-		Width:  1024,
-		Height: 768,
-		Menu:   AppMenu,
-		AssetServer: &assetserver.Options{
-			Assets: assets,
-		},
-		BackgroundColour: &options.RGBA{R: 35, G: 35, B: 35, A: 1},
-		OnStartup:        app.startup,
-		Bind: []interface{}{
-			app,
-		},
-	})
-
-	if err != nil {
-		println("Error:", err.Error())
-	}
 }
