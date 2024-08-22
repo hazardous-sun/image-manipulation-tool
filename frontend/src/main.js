60bc3985025c33cd2cc251ff9d11267a5e4259f3
solaire <141582502+hazardous-sun@users.noreply.github.com>
2024-08-22 18:49:07 -0300

    set a basic GUI for the project, now I'll move on to some basic interactions with the image

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,21 +4,11 @@ import './app.css';
 import logo from './assets/images/logo-universal.png';
 import {Greet} from '../wailsjs/go/main/App';
 
-document.querySelector('#imagesPanel').innerHTML = `
-<!--    <img id="logo" class="logo">-->
-<!--      <div class="result" id="result">Please enter your name below 👇</div>-->
-<!--      <div class="input-box" id="input">-->
-<!--        <input class="input" id="name" type="text" autocomplete="off" />-->
-<!--        <button class="btn" onclick="greet()">Greet</button>-->
-<!--      </div>-->
-<!--    </div>-->
-
-<div class="image-container centerHV">
-    <img src="src/assets/images/cat3.jpg" alt="Image 1">
-    <img src="src/assets/images/cat3.jpg" alt="Image 2">
-</div>
-`;
-document.getElementById('logo').src = logo;
+// document.querySelector('#imagesPanel').innerHTML = `
+// <!--    <img src="src/assets/images/cat3.jpg" alt="Image 1">-->
+// <!--    <img src="src/assets/images/cat3.jpg" alt="Image 2">-->
+// `;
+// document.getElementById('logo').src = logo;
 
 let nameElement = document.getElementById("name");
 nameElement.focus();
