056ede01bd3650197b52423c336cf525f3a3b724
solaire <141582502+hazardous-sun@users.noreply.github.com>
2024-08-23 10:30:27 -0300

    moved the part of the code that deletes the temp dir after closing the application to the app instance

--- gui.go
+++ gui.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"embed"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/menu"
@@ -44,16 +45,17 @@ func Run() {
 		},
 		BackgroundColour: &options.RGBA{R: 5, G: 255, B: 255, A: 1},
 		OnStartup:        app.startup,
+		OnShutdown: func(ctx context.Context) {
+			err = removeTemporaryDir()
+
+			if err != nil {
+				println("Error:", err.Error())
+			}
+		},
 		Bind: []interface{}{
 			app,
 		},
 	})
-
-	if err != nil {
-		println("Error:", err.Error())
-	}
-
-	err = removeTemporaryDir()
 
 	if err != nil {
 		println("Error:", err.Error())
