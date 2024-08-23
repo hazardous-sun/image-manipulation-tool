99b5b3a7ed8f3650e3e9b6026d49330daf8bc351
solaire <141582502+hazardous-sun@users.noreply.github.com>
2024-08-23 08:46:41 -0300

    removed calls to unused functions and assets

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,7 @@
 import './style.css';
 import './app.css';
 
-import logo from './assets/images/logo-universal.png';
-import {Greet} from '../wailsjs/go/main/App';
 import {EventsOn} from "../wailsjs/runtime";
-
-// document.querySelector('#imagesPanel').innerHTML = `
-// <!--    <img src="src/assets/images/cat3.jpg" alt="Image 1">-->
-// <!--    <img src="src/assets/images/cat3.jpg" alt="Image 2">-->
-// `;
-// document.getElementById('logo').src = logo;
 
 function setImage(path) {
     var originalImage = document.getElementById("originalImage");
