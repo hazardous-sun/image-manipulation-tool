995f880c5c50deac26134bec8740c75dc6c72444
solaire <141582502+hazardous-sun@users.noreply.github.com>
2024-08-23 17:20:07 -0300

    added a listener that will change the CSS values depending on the theme

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,51 @@ import './app.css';
 
 import {EventsOn} from "../wailsjs/runtime";
 
+EventsOn('set-theme', (data) => async function () {
+    let themeName = getThemeName()
+});
+
+async function getThemeName() {
+    try {
+        const response = await fetch('your_initial_json_file.json');
+        const data = await response.json();
+
+        const themePath = `src/assets/themes/${data.theme}`;
+        const themeResponse = await fetch(themePath);
+        const themeData = await themeResponse.json();
+
+        // Use themeData to apply the theme to your application
+        applyTheme(themeData);
+    } catch (error) {
+        console.error('Error loading theme:', error);
+    }
+}
+
+function applyTheme(themeData) {
+    document.getElementById("app")
+        .style
+        .backgroundColor = themeData["sidebar-color"]
+    document.getElementById("imagesPanel")
+        .style
+        .backgroundColor = themeData["background-color"]
+    document.querySelectorAll('.sideBar, li')
+        .forEach(element => {
+            element.style.borderColor = themeData["sidebar-border-color"]
+        });
+    document.querySelectorAll('.sideBar, ul, li, a')
+        .forEach(element => {
+            element.style.color = themeData["font-color"]
+        })
+    document.querySelectorAll('.sideBar, ul, li, ul.dropdown')
+        .forEach(element => {
+            element.style.backgroundColor = themeData["dropdown-menu-color"]
+        })
+}
+
+EventsOn('set-image', (data) => {
+    setImage(data.fileExt);
+});
+
 function setImage(fileExt) {
     var originalImage = document.getElementById("originalImage");
     var previewImage = document.getElementById("previewImage");
@@ -10,9 +55,4 @@ function setImage(fileExt) {
     originalImage.src = "src/assets/temp/origin" + fileExt;
     previewImage.src = "src/assets/temp/prev" + fileExt;
 }
-
-EventsOn('set-image', (data) => {
-    console.log(data.fileExt);
-    setImage(data.fileExt);
-});
 
