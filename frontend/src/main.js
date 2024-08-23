41ffb1932811c6b53e5da1fc878ee36eedadfdd2
solaire <141582502+hazardous-sun@users.noreply.github.com>
2024-08-23 12:41:49 -0300

    updated the listeners and paths for setting the images

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,16 +3,16 @@ import './app.css';
 
 import {EventsOn} from "../wailsjs/runtime";
 
-function setImage(path) {
+function setImage(fileExt) {
     var originalImage = document.getElementById("originalImage");
     var previewImage = document.getElementById("previewImage");
 
-    originalImage.src = path;
-    previewImage.src = path;
+    originalImage.src = "frontend/src/assets/temp/origin" + fileExt;
+    previewImage.src = "frontend/src/assets/temp/origin" + fileExt;
 }
 
 EventsOn('set-image', (data) => {
-    console.log(data.image);
-    setImage("src/assets/temp/" + data.image);
+    console.log(data.fileExt);
+    setImage(data.fileExt);
 });
 
