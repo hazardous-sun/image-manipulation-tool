a5fc0c13edc21154ce2de993e586984fd85231f8
solaire <141582502+hazardous-sun@users.noreply.github.com>
2024-08-23 16:37:40 -0300

    adjusted the path to the images

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,8 +7,8 @@ function setImage(fileExt) {
     var originalImage = document.getElementById("originalImage");
     var previewImage = document.getElementById("previewImage");
 
-    originalImage.src = "frontend/src/assets/temp/origin" + fileExt;
-    previewImage.src = "frontend/src/assets/temp/origin" + fileExt;
+    originalImage.src = "src/assets/temp/origin" + fileExt;
+    previewImage.src = "src/assets/temp/prev" + fileExt;
 }
 
 EventsOn('set-image', (data) => {
