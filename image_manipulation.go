22a6808641418430af7dc9e28835c69d7e94068e
solaire <contact@unharmfulhazard.com>
2024-08-27 08:57:30 -0300

    Started to add the geometric transformations functions.

--- image_manipulation.go
+++ image_manipulation.go
@@ -217,9 +217,34 @@ func saveImage(path string, fileExt string, img image.Image) error {
 	return nil
 }
 
+// Geometric transformations -------------------------------------------------------------------------------------------
+
+func transformImage(img image.Image, matrix [][]int) image.Image {
+	transformedImage := image.NewRGBA(img.Bounds())
+
+	/*
+						[
+							[v1, v2, v3]
+		[Xf, Yf, 1] =		[v1, v2, v3]   *   [X1, Y1, 1]
+							[v1, v2, v3]
+						]
+	*/
+
+	return transformedImage
+}
+
+func getTranslationMatrix(x int, y int) [][]int {
+	matrix := [][]int{
+		{1, 0, x},
+		{0, 1, y},
+		{0, 0, 1},
+	}
+	return matrix
+}
+
 // Filters -------------------------------------------------------------------------------------------------------------
 
-// Grayscale
+// -------- Grayscale
 /*
 Applies the grayscale filter to an image.
 */
