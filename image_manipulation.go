1e96d21481ddf3156a2e2c478c34de5b368616fe
solaire <141582502+hazardous-sun@users.noreply.github.com>
2024-08-27 09:06:08 -0300

    Moved all the code regarding filters to the 'filtes.go' file.

--- image_manipulation.go
+++ image_manipulation.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 	"image"
-	color2 "image/color"
-	"image/draw"
 	"image/jpeg"
 	"image/png"
 	"io"
@@ -240,33 +238,6 @@ func getTranslationMatrix(x int, y int) [][]int {
 		{0, 0, 1},
 	}
 	return matrix
-}
-
-// Filters -------------------------------------------------------------------------------------------------------------
-
-// -------- Grayscale
-/*
-Applies the grayscale filter to an image.
-*/
-func filterGrayScale(img image.Image) image.Image {
-	grayImage := image.NewGray(img.Bounds())
-	draw.Draw(grayImage, grayImage.Bounds(), img, image.Point{}, draw.Src)
-	return grayImage
-}
-
-func oldFilterGrayScale(img image.Image) image.Image {
-	grayImage := image.NewGray(img.Bounds())
-	for x := 0; x < img.Bounds().Dx(); x++ {
-		for y := 0; y < img.Bounds().Dy(); y++ {
-			r, g, b, _ := img.At(x, y).RGBA()
-			grayIntensity := (r + g + b) / 3
-			grayImage.Set(x, y, color2.Gray{
-				Y: uint8(grayIntensity),
-			})
-		}
-	}
-	fmt.Println(grayImage.GrayAt(10, 10))
-	return grayImage
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
