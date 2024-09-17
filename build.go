9d8bb40d5fb71064ddb31c421cfad9e690c3d5bc
solaire <141582502+hazardous-sun@users.noreply.github.com>
2024-09-16 21:04:26 -0300

    ✍ Refactoring: Separated the code for each menu button into a function.
    This will improve the readability and management of the code.

--- build.go
+++ build.go
@@ -201,13 +201,95 @@ func setMenu(app *App) *menu.Menu {
 func setFileMenu(app *App, AppMenu *menu.Menu) {
 	// File
 	FileMenu := AppMenu.AddSubmenu("File")
+
 	// -- Open image
 	FileMenu.AddText("Open", keys.CmdOrCtrl("o"), func(_ *menu.CallbackData) {
-		// Try to collect current working directory
+		menuOpenImage(app)
+	})
+	FileMenu.AddSeparator()
+
+	// -- Save image
+	FileMenu.AddText("Save", keys.CmdOrCtrl("s"), func(_ *menu.CallbackData) {
+		menuSaveImage(app)
+	})
+	FileMenu.AddSeparator()
+
+	// -- About
+	FileMenu.AddText("About", keys.CmdOrCtrl("f1"), func(_ *menu.CallbackData) {
+		menuAbout(app)
+	})
+	FileMenu.AddSeparator()
+
+	// -- Exit
+	FileMenu.AddText("Exit", keys.CmdOrCtrl("q"), func(_ *menu.CallbackData) {
+		menuExit(app)
+	})
+}
+
+func menuOpenImage(app *App) {
+	// Try to collect current working directory
+	cwd, err := os.Getwd()
+
+	if err != nil {
+		println(pError()+" when CWD collection:", err.Error())
+		if goRuntime.GOOS == "windows" {
+			cwd = "%USERPROFILE%"
+		} else {
+			cwd = "~"
+		}
+	}
+
+	path, err := runtime.OpenFileDialog(
+		app.ctx,
+		runtime.OpenDialogOptions{
+			DefaultDirectory: cwd,
+			DefaultFilename:  "",
+			Title:            "Select image",
+			Filters: []runtime.FileFilter{
+				{
+					DisplayName: "Image Files (*.gif, *.jpeg, *.jpg, *.png)",
+					Pattern:     "*.gif;*.jpeg;*.jpg;*.png;",
+				},
+			},
+			ShowHiddenFiles:            false,
+			CanCreateDirectories:       false,
+			ResolvesAliases:            false,
+			TreatPackagesAsDirectories: false,
+		},
+	)
+
+	if err != nil {
+		println(pError()+" during dialog:", err.Error())
+		return
+	}
+
+	setOriginPrev(app, path)
+
+}
+
+func menuSaveImage(app *App) {
+	runtime.EventsEmit(app.ctx, "save-image-request", nil)
+
+	runtime.EventsOn(app.ctx, "save-image-response", func(optionalData ...interface{}) {
+		urlString, err := url.Parse(optionalData[0].(string))
+		filename := filepath.Base(urlString.Path)
+
+		if filename == "." {
+			runtime.MessageDialog(app.ctx, runtime.MessageDialogOptions{
+				Type:          runtime.ErrorDialog,
+				Title:         "Error",
+				Message:       "You have no image open at the moment.",
+				DefaultButton: "Back",
+			})
+			return
+		}
+
+		imageToSavePath := filepath.Join("frontend", "src", "assets", "temp", "prev", filename)
+
 		cwd, err := os.Getwd()
 
 		if err != nil {
-			println(pError()+" when CWD collection:", err.Error())
+			println(pError()+" during CWD collection:", err.Error())
 			if goRuntime.GOOS == "windows" {
 				cwd = "%USERPROFILE%"
 			} else {
@@ -215,134 +297,75 @@ func setFileMenu(app *App, AppMenu *menu.Menu) {
 			}
 		}
 
-		path, err := runtime.OpenFileDialog(
+		newFilePath, err := runtime.SaveFileDialog(
 			app.ctx,
-			runtime.OpenDialogOptions{
-				DefaultDirectory: cwd,
-				DefaultFilename:  "",
-				Title:            "Select image",
-				Filters: []runtime.FileFilter{
-					{
-						DisplayName: "Image Files (*.gif, *.jpeg, *.jpg, *.png)",
-						Pattern:     "*.gif;*.jpeg;*.jpg;*.png;",
-					},
-				},
-				ShowHiddenFiles:            false,
-				CanCreateDirectories:       false,
-				ResolvesAliases:            false,
+			runtime.SaveDialogOptions{
+				DefaultDirectory:           cwd,
+				DefaultFilename:            filepath.Ext(filename),
+				Title:                      "Save image",
+				Filters:                    nil,
+				ShowHiddenFiles:            true,
+				CanCreateDirectories:       true,
 				TreatPackagesAsDirectories: false,
 			},
 		)
 
+		img, err := loadImage(imageToSavePath)
+
 		if err != nil {
-			println(pError()+" during dialog:", err.Error())
+			println(pError()+" when loading image:", err.Error())
 			return
 		}
 
-		setOriginPrev(app, path)
+		err = saveImage(newFilePath, filepath.Ext(imageToSavePath), img)
+
+		if err != nil {
+			println(pError()+" when saving image:", err.Error())
+			return
+		}
+
+		runtime.EventsOff(app.ctx, "receive-prev")
 	})
-	FileMenu.AddSeparator()
-	// -- Save image
-	FileMenu.AddText("Save", keys.CmdOrCtrl("s"), func(_ *menu.CallbackData) {
-		runtime.EventsEmit(app.ctx, "save-image-request", nil)
 
-		runtime.EventsOn(app.ctx, "save-image-response", func(optionalData ...interface{}) {
-			urlString, err := url.Parse(optionalData[0].(string))
-			filename := filepath.Base(urlString.Path)
+}
 
-			if filename == "." {
-				runtime.MessageDialog(app.ctx, runtime.MessageDialogOptions{
-					Type:          runtime.ErrorDialog,
-					Title:         "Error",
-					Message:       "You have no image open at the moment.",
-					DefaultButton: "Back",
-				})
-				return
-			}
-
-			imageToSavePath := filepath.Join("frontend", "src", "assets", "temp", "prev", filename)
-
-			cwd, err := os.Getwd()
-
-			if err != nil {
-				println(pError()+" during CWD collection:", err.Error())
-				if goRuntime.GOOS == "windows" {
-					cwd = "%USERPROFILE%"
-				} else {
-					cwd = "~"
-				}
-			}
-
-			newFilePath, err := runtime.SaveFileDialog(
-				app.ctx,
-				runtime.SaveDialogOptions{
-					DefaultDirectory:           cwd,
-					DefaultFilename:            filepath.Ext(filename),
-					Title:                      "Save image",
-					Filters:                    nil,
-					ShowHiddenFiles:            true,
-					CanCreateDirectories:       true,
-					TreatPackagesAsDirectories: false,
-				},
-			)
-
-			img, err := loadImage(imageToSavePath)
-
-			if err != nil {
-				println(pError()+" when loading image:", err.Error())
-				return
-			}
-
-			err = saveImage(newFilePath, filepath.Ext(imageToSavePath), img)
-
-			if err != nil {
-				println(pError()+" when saving image:", err.Error())
-				return
-			}
-
-			runtime.EventsOff(app.ctx, "receive-prev")
-		})
+func menuAbout(app *App) {
+	_, err := runtime.MessageDialog(app.ctx, runtime.MessageDialogOptions{
+		Type:          runtime.InfoDialog,
+		Title:         "About",
+		Message:       "This is an image manipulation tool written in Go using Wails framework to Run the frontend.",
+		DefaultButton: "Back",
 	})
-	FileMenu.AddSeparator()
-	// -- About
-	FileMenu.AddText("About", keys.CmdOrCtrl("f1"), func(_ *menu.CallbackData) {
-		_, err := runtime.MessageDialog(app.ctx, runtime.MessageDialogOptions{
-			Type:          runtime.InfoDialog,
-			Title:         "About",
-			Message:       "This is an image manipulation tool written in Go using Wails framework to Run the frontend.",
-			DefaultButton: "Back",
+
+	if err != nil {
+		println(pError()+" during dialog: %w", err)
+	}
+}
+
+func menuExit(app *App) {
+	if UnsavedProgress {
+		choice, err := runtime.MessageDialog(app.ctx, runtime.MessageDialogOptions{
+			Type:    runtime.WarningDialog,
+			Title:   "Warning",
+			Message: "Do you really wish to quit? All unsaved progress will be lost.",
+			Buttons: []string{"Cancel", "Quit"},
 		})
 
 		if err != nil {
-			println(pError()+" during dialog: %w", err)
-		}
-	})
-	FileMenu.AddSeparator()
-	// -- Exit
-	FileMenu.AddText("Exit", keys.CmdOrCtrl("q"), func(_ *menu.CallbackData) {
-		if UnsavedProgress {
-			choice, err := runtime.MessageDialog(app.ctx, runtime.MessageDialogOptions{
-				Type:    runtime.WarningDialog,
-				Title:   "Warning",
-				Message: "Do you really wish to quit? All unsaved progress will be lost.",
-				Buttons: []string{"Cancel", "Quit"},
-			})
-
-			if err != nil {
-				println(pError()+" during dialog:", err)
-				return
-			}
-
-			switch choice {
-			case "Quit":
-				runtime.Quit(app.ctx)
-			case "Cancel":
-				return
-			default:
-				return
-			}
+			println(pError()+" during dialog:", err)
+			return
 		}
 
-		runtime.Quit(app.ctx)
-	})
+		switch choice {
+		case "Quit":
+			runtime.Quit(app.ctx)
+		case "Cancel":
+			return
+		default:
+			return
+		}
+	}
+
+	runtime.Quit(app.ctx)
+
 }
