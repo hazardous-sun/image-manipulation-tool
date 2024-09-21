fa08d28e3951a54ccc11781fe3ac62924a132f7b
solaire <contact@unharmfulhazard.com>
2024-09-21 16:00:41 -0300

    I'll implement my own method of applying the grayscale filter.
    At the moment, the grayscale filter is making the image unrecognizable.

--- filters.go
+++ filters.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"image"
-	"image/draw"
+	"image/color"
 )
 
 // Filters -------------------------------------------------------------------------------------------------------------
@@ -10,23 +11,24 @@ import (
 // -------- Grayscale
 
 // Removes the color channels of an image and returns an image with only shades of gray.
-func filterGrayScale(img image.Image) image.Image {
-	grayImage := image.NewGray(img.Bounds())
-	draw.Draw(grayImage, grayImage.Bounds(), img, image.Point{}, draw.Src)
-	return grayImage
-}
-
-//func oldFilterGrayScale(img image.Image) image.Image {
+//func invalidFilterGrayScale(img image.Image) image.Image {
 //	grayImage := image.NewGray(img.Bounds())
-//	for x := 0; x < img.Bounds().Dx(); x++ {
-//		for y := 0; y < img.Bounds().Dy(); y++ {
-//			r, g, b, _ := img.At(x, y).RGBA()
-//			grayIntensity := (r + g + b) / 3
-//			grayImage.Set(x, y, color2.Gray{
-//				Y: uint8(grayIntensity),
-//			})
-//		}
-//	}
-//	fmt.Println(grayImage.GrayAt(10, 10))
+//	draw.Draw(grayImage, grayImage.Bounds(), img, image.Point{}, draw.Src)
 //	return grayImage
 //}
+
+// Removes the color channels of an image and returns an image with only shades of gray.
+func filterGrayScale(img image.Image) image.Image {
+	grayImage := image.NewGray(img.Bounds())
+	for x := 0; x < img.Bounds().Dx(); x++ {
+		for y := 0; y < img.Bounds().Dy(); y++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+			grayIntensity := (r + g + b) / 3
+			grayImage.Set(x, y, color.Gray{
+				Y: uint8(grayIntensity),
+			})
+		}
+	}
+	fmt.Println(grayImage.GrayAt(10, 10))
+	return grayImage
+}
