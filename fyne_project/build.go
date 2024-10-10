2c3c67b7bc429989c0c155b291facacab2b447dc
solaire <141582502+hazardous-sun@users.noreply.github.com>
2024-10-10 13:19:44 -0300

    Developed the basics for the images container and the side bar.

--- fyne_project/build.go
+++ fyne_project/build.go
@@ -4,11 +4,12 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/widget"
 	"image-manipulation-tool/fyne_project/models"
 )
 
 func Build(a fyne.App) {
-	project := models.NewProject()
 	w := a.NewWindow("Image Manipulation Tool")
 	w.Resize(fyne.NewSize(800, 600))
 
@@ -24,18 +25,92 @@ func Build(a fyne.App) {
 		),
 	)
 
+	imgsCtr := initializeImgsCtr()
+	sideBar := initializeSideBar()
+
+	appCtr := container.NewBorder(
+		nil, nil,
+		imgsCtr,
+		sideBar,
+	)
+
+	w.SetContent(appCtr)
+	w.ShowAndRun()
+}
+
+func initializeImgsCtr() fyne.CanvasObject {
+	// initialize the Project instance
+	project := models.NewProject()
+
+	// initialize the original image canvas
 	originalImage := project.GetOriginal()
 	originalImageCanvas := canvas.NewImageFromImage(originalImage)
 	originalImageCanvas.FillMode = canvas.ImageFillOriginal
 
+	// build a container for the original image
+	originalImageCtr := container.NewBorder(
+		widget.NewLabel("Original image"),
+		nil, nil, nil,
+		originalImageCanvas,
+	)
+
+	// initialize the preview image canvas
 	previewImage := project.GetPreview()
 	previewImageCanvas := canvas.NewImageFromImage(previewImage)
 	previewImageCanvas.FillMode = canvas.ImageFillOriginal
-	imgsCtr := container.NewBorder(
-		nil, nil,
-		originalImageCanvas,
+
+	// build a container for the preview image
+	previewImageCtr := container.NewBorder(
+		widget.NewLabel("Preview image"),
+		nil, nil, nil,
 		previewImageCanvas,
 	)
-	w.SetContent(imgsCtr)
-	w.ShowAndRun()
+
+	return container.NewBorder(
+		nil, nil,
+		originalImageCtr,
+		previewImageCtr,
+	)
+}
+
+func initializeSideBar() fyne.CanvasObject {
+	btnsArr := []models.EditButton{
+		models.NewEditButton("Resize", func() {}),
+		models.NewEditButton("Rotate", func() {}),
+		models.NewEditButton("Translate", func() {}),
+		models.NewEditButton("Horizontal mirroring", func() {}),
+		models.NewEditButton("Vertical mirroring", func() {}),
+	}
+
+	btnsData := binding.NewUntypedList()
+	for _, btn := range btnsArr {
+		_ = btnsData.Append(btn)
+	}
+
+	sideBar := widget.NewAccordion(
+		widget.NewAccordionItem(
+			"Geometric trasnformations",
+			widget.NewListWithData(
+				btnsData,
+				func() fyne.CanvasObject {
+					return widget.NewButton("", func() {})
+				},
+				func(di binding.DataItem, object fyne.CanvasObject) {
+					objBtn := object.(*widget.Button)
+					temp, _ := di.(binding.Untyped).Get()
+					diBtn := temp.(models.EditButton)
+					objBtn.SetText(diBtn.Text)
+					objBtn.OnTapped = diBtn.Behavior
+				},
+			),
+		),
+		widget.NewAccordionItem("Filters", widget.NewLabel("Teste")),
+		widget.NewAccordionItem("Mathematical morphology", widget.NewLabel("Teste")),
+		widget.NewAccordionItem("Feature extraction", widget.NewLabel("Teste")),
+	)
+
+	return container.NewBorder(
+		nil, nil, nil,
+		sideBar,
+	)
 }
