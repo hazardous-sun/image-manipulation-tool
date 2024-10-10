4987a96e166193aa031212d82881cac2be3c6ee2
solaire <141582502+hazardous-sun@users.noreply.github.com>
2024-10-10 11:24:52 -0300

    Added functions to load images both to an instance of image.Image and to an array of bytes.

---
+++ fyne_project/file_handling/image_handling.go
@@ -0,0 +1,73 @@
+package file_handling
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image-manipulation-tool/fyne_project"
+	"image/jpeg"
+	"image/png"
+	"os"
+	"path/filepath"
+)
+
+// LoadImage :
+// Decodes an image file and transform it into an "image.Image".
+func LoadImage(path string) (image.Image, error) {
+	// Open the file
+	file, err := os.Open(path)
+
+	if err != nil {
+		return nil, err
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			println(fyne_project.RError()+" when closing file '%s': %w", path, err)
+		}
+	}(file)
+
+	// Check if the file is a supported image
+	fileExt := filepath.Ext(path)
+	switch fileExt {
+	case ".jpg", ".jpeg":
+		image.RegisterFormat("jpeg", "jpeg", jpeg.Decode, jpeg.DecodeConfig)
+		return jpeg.Decode(file)
+	case ".png":
+		image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
+		return png.Decode(file)
+	default:
+		return image.Image(nil), fmt.Errorf("unsupported image format: '%s'", fileExt)
+	}
+}
+
+// LoadImageToBytes :
+// Decodes an image file and transform it into an array of bytes.
+func LoadImageToBytes(path string) ([]byte, error) {
+	file, err := os.Open(path)
+
+	if err != nil {
+		return nil, fmt.Errorf(fyne_project.RError()+" when opening file: %w", err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			println(fyne_project.RError()+" when closing file '%s': %w", path, err)
+		}
+	}(file)
+
+	img, _, err := image.Decode(file)
+
+	if err != nil {
+		return nil, fmt.Errorf(fyne_project.RError()+" when decoding file: %w", err)
+	}
+
+	buf := &bytes.Buffer{}
+	err = png.Encode(buf, img)
+
+	if err != nil {
+		return nil, fmt.Errorf(fyne_project.RError()+" when encoding file to bytes: %w", err)
+	}
+
+	return buf.Bytes(), nil
+}
