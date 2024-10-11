1376d955148f19ddce9303f7903427bf2f39048b
solaire <141582502+hazardous-sun@users.noreply.github.com>
2024-10-11 12:47:45 -0300

    Added the ChangesStack data structure to Project for handling the different images.

--- models/project.go
+++ models/project.go
@@ -27,58 +27,91 @@ func (p *Project) GetOriginal() image.Image {
 }
 
 func (p *Project) GetPreview() image.Image {
-	if len(p.previewImage) == 0 {
-		return nil
+	return p.previewImage
+}
+
+func (p *Project) PreviousPreviewImage() (image.Image, error) {
+	// get the previous preview image
+	previousPreview := p.previousStates.Pop()
+
+	// check if there is a previous preview image
+	if previousPreview == nil {
+		return p.previewImage, fmt.Errorf("no previous state")
 	}
-	return p.previewImage[p.currentVersion]
+
+	// get the original image for the previous preview
+	previousOriginal := p.previousStates.Pop()
+
+	// if no original image exist, it means the original should be set the same as the preview
+	if previousOriginal == nil {
+		p.originalImage = previousPreview.(image.Image)
+	} else {
+		// if there IS an original image for the preview, pass it to p.originalImage
+		p.originalImage = previousOriginal.(image.Image)
+
+		// and then send it back to p.previousStates
+		p.previousStates.Push(previousOriginal)
+	}
+
+	// pass the current image of p.previewImage to the stack of p.nextStates
+	p.nextStates.Push(p.previewImage)
+	p.previewImage = previousPreview.(image.Image)
+	return p.previewImage, nil
 }
 
 func (p *Project) NextPreviewImage() (image.Image, error) {
-	if p.currentVersion >= p.versions {
-		return nil, fmt.Errorf("no preview images available")
+	nextPreview := p.nextStates.Pop()
+
+	if nextPreview == nil {
+		return p.previewImage, fmt.Errorf("no next state")
 	}
 
-	p.originalImage = p.previewImage[p.currentVersion]
-	p.currentVersion++
-	return p.GetPreview(), nil
-}
-
-func (p *Project) PreviousPreviewImage() image.Image {
-	if p.currentVersion == 0 {
-		return p.previewImage[0]
-	}
-
-	index := p.currentVersion - 1
-
-	if index > 0 {
-		p.originalImage = p.previewImage[index-1]
-	} else {
-		p.originalImage = p.previewImage[index]
-	}
-
-	return p.previewImage[index]
+	p.previousStates.Push(p.previewImage)
+	p.previewImage = nextPreview.(image.Image)
+	return p.previewImage, nil
 }
 
 // Updating Values -----------------------------------------------------------------------------------------------------
 
 func (p *Project) AddPreviewImage(img image.Image) {
-	if p.currentVersion < p.versions {
-		p.previewImage = append(p.previewImage[:p.currentVersion], img)
-		p.currentVersion++
-		p.versions = p.currentVersion
-	} else {
-		p.previewImage = append(p.previewImage, img)
-		p.currentVersion++
+	// if p.nextStates is empty, it means that there is no work to reset
+	if p.nextStates.Empty() {
+		// set p.originalImage to p.previewImage
+		p.originalImage = p.previewImage
+		// push p.previewImage to p.previousStates
+		p.previousStates.Push(p.previewImage)
+		// set p.previewImage to img
+		p.previewImage = img
+		// increment p.versions
 		p.versions++
+	} else {
+		// if the stack is not empty, p.nextStates should be cleared, because the work that had been done needs to be
+		// overwritten
+
+		// collect the amount of elements in p.nextStates to calculate the new versions amount
+		previousVersions := p.nextStates.Length()
+		// clear p.nextStates stack
+		p.nextStates.Clear()
+		// set p.originalImage to p.previewImage
+		p.originalImage = p.previewImage
+		// push p.previewImage to p.previousStates
+		p.previousStates.Push(p.previewImage)
+		// set p.previewImage to img
+		p.previewImage = img
+		// calculate the new p.versions value
+		p.versions -= previousVersions + 1
 	}
-	p.originalImage = p.PreviousPreviewImage()
 }
 
 func (p *Project) LoadNewImage(img image.Image) {
-	p.versions = 0
-	p.currentVersion = 0
+	// reset the stacks and the versions count
+	p.versions = 1
+	p.previousStates.Clear()
+	p.nextStates.Clear()
+
+	// pass img to the project
 	p.originalImage = img
-	p.previewImage = []image.Image{img}
+	p.previewImage = img
 }
 
 // Constructor ---------------------------------------------------------------------------------------------------------
@@ -87,8 +120,9 @@ func NewProject() *Project {
 	initialImg := image.NewRGBA(image.Rect(0, 0, 512, 512))
 	return &Project{
 		versions:       1,
-		currentVersion: 0,
 		originalImage:  initialImg,
-		previewImage:   []image.Image{initialImg},
+		previewImage:   initialImg,
+		previousStates: ChangesStack{},
+		nextStates:     ChangesStack{},
 	}
 }
