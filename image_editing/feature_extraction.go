bee2aa1dc35c39d77473749854783e3bfd3a2ad0
solaire <contact@unharmfulhazard.com>
2024-12-02 09:53:04 -0300

    The domino's dot extaction challenge seems to be working quite nice.

--- image_editing/feature_extraction.go
+++ image_editing/feature_extraction.go
@@ -8,6 +8,8 @@ import (
 
 func FeatureExtractCountDominoDots(img image.Image) image.Image {
 	found := false
+	alreadyIncremented := false
+	rowCompleted := 0
 	matrix := generateMatrix(img)
 	count := 0
 
@@ -35,16 +37,21 @@ func FeatureExtractCountDominoDots(img image.Image) image.Image {
 	resultImg := image.NewRGBA(img.Bounds())
 
 	// iterate over all x and y, when a white spot is found, increase count
-	for x := 0; x < img.Bounds().Dx(); x++ {
-		for y := 0; y < img.Bounds().Dy(); y++ {
+	for y := 0; y < img.Bounds().Dx(); y++ {
+		for x := 0; x < img.Bounds().Dy(); x++ {
 			r, _, _, _ := tempImage.At(x, y).RGBA()
 			if found == false && (r/256) >= 100 {
-				if noWhiteNeighbours(x, y, matrix) {
+				if alreadyIncremented == false {
+					alreadyIncremented = true
 					count++
-					found = true
+					rowCompleted++
 				}
+				found = true
 			} else if found == true && (r/256) == 0 {
 				found = false
+				if noWhiteNeighboursUpdated(x, y, tempImage) {
+					alreadyIncremented = false
+				}
 			}
 			if (r / 256) >= 100 {
 				matrix[x][y] = 1
@@ -55,6 +62,11 @@ func FeatureExtractCountDominoDots(img image.Image) image.Image {
 					A: 255,
 				}
 				resultImg.Set(x, y, pixel)
+			}
+			if rowCompleted == 2 {
+				rowCompleted = 0
+				y += 5
+				break
 			}
 		}
 	}
@@ -76,8 +88,8 @@ func generateMatrix(img image.Image) [][]int {
 }
 
 func noWhiteNeighbours(x, y int, matrix [][]int) bool {
-	for innerX := -1; innerX < 1; innerX++ {
-		for innerY := -1; innerY < 1; innerY++ {
+	for innerX := -2; innerX < 2; innerX++ {
+		for innerY := -2; innerY < 2; innerY++ {
 			if matrix[x-innerX][y-innerY] == 1 {
 				return false
 			}
@@ -86,7 +98,40 @@ func noWhiteNeighbours(x, y int, matrix [][]int) bool {
 	return true
 }
 
+func noWhiteNeighboursUpdated(x, y int, img image.Image) bool {
+	for innerX := -1; innerX < 1; innerX++ {
+		for innerY := -1; innerY < 1; innerY++ {
+			r, _, _, _ := img.At(x-innerX, y-innerY).RGBA()
+			newR := uint8(r / 256)
+			if newR >= 255 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
 /*
+grayscale
+sobel 255
+dilation
+erosion
+erosion
+dilation
+dilation
+dilation
+
+iterate over all x and y, when a white spot is found, increase count
+
+
+x = 52
+y = 24
+
+52 - -1 = 53
+52 - 0 = 52
+52 - 1 = 51
+
+
 Matrix with the size of the image
 Set all values to 0
 When a white pixel is found, check if the found flag is set, if it is not, check if there are white neighbour pixels
